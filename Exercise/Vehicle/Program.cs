6596903c48dab3b3d77df6af0385af8c02a8596e
phenkelmann <phenkelmann@online.de>
2017-03-17 16:33:33 +0100

    Add Vehicle Member and Propertys

---
+++ Exercise/Vehicle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle seat = new Vehicle("Seat", "black", 80);
+            Console.WriteLine(seat.Brand + " " + seat.HP);
+            seat.HP = 220;
+            Console.WriteLine(seat.KWH);
+        }
+    }
+}
---
+++ Exercise/Vehicle/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Vehicle
+    {
+        private string brand;
+        private string color;
+        private int kwh;
+
+        private readonly double CONVERSION = 1.3428;
+
+        public string Brand
+        {
+            get { return brand; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+        }
+
+        public int HP
+        {
+            get { return (int)(kwh * CONVERSION); }
+            set { kwh = (int)(value / CONVERSION); }
+        }
+
+        public int KWH
+        {
+            get { return kwh; }
+            set { kwh = value; }
+        }
+
+        public Vehicle(string brand, string color, int kwh)
+        {
+            this.brand = brand;
+            this.color = color;
+            this.kwh = kwh;
+        }
+
+
+
+
+    }
+}
