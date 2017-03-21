d0e83d665f769f7c37a696c802b853ee7620d11e
phenkelmann <phenkelmann@DEV-HENKELMANN>
2017-03-21 12:59:21 +0100

    Add Queue

---
+++ Exercise/Queue/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(0);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+            
+            queue.printQueue();
+            Console.WriteLine("Dequeue: " + queue.Dequeue());
+            Console.WriteLine("Dequeue: " + queue.Dequeue());
+            queue.Enqueue(213);
+            Console.WriteLine("Dequeue: " + queue.Dequeue());
+            queue.printQueue();
+        }
+    }
+}
---
+++ Exercise/Queue/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        private object[] elements = new object[10];
+        private int backValue = 0;
+        private int frontValue = 0;
+
+        public int back
+        {
+            set
+            {
+                backValue = value % 10;
+            }
+            get
+            {
+                return backValue;
+            }
+        }
+
+        private int front
+        {
+            set
+            {
+                frontValue = value % 10;
+            }
+            get
+            {
+                return frontValue;
+            }
+        }
+
+        private int getNextIndex()
+        {
+            if((back == front) && (elements[front] != null)) ++front;
+            return back;
+        }
+
+        public void Enqueue(object element)
+        {
+            elements[getNextIndex()] = element;
+            ++back;
+        }
+
+        public object Dequeue()
+        {
+            object obj = elements[front];
+            elements[front] = null;
+            ++front;
+            return obj;
+        }
+
+        public Queue()
+        {
+           
+
+        }
+
+        public void printQueue()
+        {
+            while(elements[front] != null)
+            {
+                Console.WriteLine(elements[front]);
+                ++front;
+                if (front == back && elements[front] != null) break;
+
+            }
+        }
+    }
+}
