1df9e8a3f8f781eaa5a95a319b774422b831fdd8
phenkelmann <phenkelmann@DEV-HENKELMANN>
2017-03-22 11:35:04 +0100

    Add Loop

---
+++ Exercise/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxValue(1, 3, 2));
+            Console.WriteLine(SwitchCase("one"));
+            ForEach(new int[]{ 1,2,3,4,5});
+        }
+
+        static int MaxValue(int a, int b, int c) // if
+        {
+            if (a > b && a > c) return a;
+            else if (b > a && b > c) return b;
+            else return c;
+        }
+
+        static int SwitchCase(string value)
+        {
+            switch (value)
+            {
+                case ("one"): return 1;
+                case ("two"): return 2;
+                case ("three"): return 3;
+                default: return -1;
+            }
+
+        }
+
+        static void ForEach(int[] array)
+        {
+            foreach(int obj in array)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+        }
+        
+
+        }
+
+
+    }
