4e2e8e1ed61ecaefb30f259e95659fefb7885295
phenkelmann <phenkelmann@DEV-HENKELMANN>
2017-03-22 09:49:16 +0100

    Add Namespaces

---
+++ Exercise/Namespaces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    public struct Time
+    {
+        int hour;
+        int min;
+        public Time(int hour, int min)
+        {
+            this.hour = hour;
+            this.min = min;
+        }
+    }
+
+    namespace B
+    {
+        class Programm
+        {
+            static void Main(string[] args)
+            {
+                Time time = new A.Time(10, 05);
+            }
+        }
+    }
+
+}
