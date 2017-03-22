c6123b4feb1f7055abb74ea20cc2c7bcf9f38e46
phenkelmann <phenkelmann@DEV-HENKELMANN>
2017-03-22 09:37:12 +0100

    Add TypeInfoAtRuntime

---
+++ Exercise/TypeInfoAtRuntime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInfoAtRuntime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 65;
+            PrintInfo(a);
+            PrintInfo(new Program());
+        }
+
+        static void PrintInfo(object obj)
+        {
+            Type t = obj.GetType();
+            PrintString(t.FullName);
+            PrintString(t.Name);
+            PrintString(t.Namespace);
+            PrintString(t.Assembly.ToString());                        
+        }
+
+        static void PrintString(string s)
+        {
+            if(s != null)
+                Console.WriteLine(s);
+        }
+    }
+}
