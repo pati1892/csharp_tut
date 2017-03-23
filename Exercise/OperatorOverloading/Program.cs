948d6078c9d1f4e42f4d57a2f7b4217d8f8599f2
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2017-03-23 21:49:05 +0100

    Add Indexer

---
+++ Exercise/OperatorOverloading/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        //START INDEXER
+        int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
+        enum Days
+        {
+            One, Two, Three, Four, Five
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value; 
+            }
+        }
+
+        public int this[int index, int mul]
+        {
+            get
+            {
+                return array[index] * mul;
+            }
+        }
+
+        public string this[string day]
+        {
+            get {
+                Days d = (Days)Enum.Parse(typeof(Days), day);
+                return d.ToString();    
+            }            
+        }
+        //END INDEXER
+    
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            Console.WriteLine(prog[1]);
+            Console.WriteLine(prog[2, 2]);
+            Console.WriteLine(prog["Three"]);
+
+        }
+    }
+}
