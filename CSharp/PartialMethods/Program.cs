ebd379d3326eaeed84385b252b486ffc9ea95670
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2017-03-25 22:29:29 +0100

    Add PartialClass, PartialMethods without implemention

---
+++ CSharp/PartialMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialMethods
+{
+    class Program
+    {
+
+        //Have to implement
+        static void Main(string[] args)
+        {
+        }
+    }
+}
