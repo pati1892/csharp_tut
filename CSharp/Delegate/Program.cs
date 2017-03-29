a29e37ba5242c49451f1c2635780727ce23328d1
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2017-03-29 22:48:55 +0200

    Add Delegate

---
+++ CSharp/Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        //ist ein Methodentyp, dient zur deklaration von variablen, in denen Methoden gespeichert werden können
+
+        delegate void Notifier(String sender);
+
+        static void SayHello(String sender)
+        {
+            Console.WriteLine(sender);
+        }
+
+        static void SayGoodBye(String sender)
+        {
+            Console.WriteLine(sender);
+        }
+
+        static void Main(string[] args)
+        {
+            Notifier notify = new Notifier(SayHello);
+            notify("Hallo");
+        }
+    }
+}
