a31de30ba3bf3e2967b68907961f66af454b920e
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2017-04-03 22:29:57 +0200

    Update delegates

--- CSharp/Delegate/Program.cs
+++ CSharp/Delegate/Program.cs
@@ -11,21 +11,49 @@ namespace Delegate
         //ist ein Methodentyp, dient zur deklaration von variablen, in denen Methoden gespeichert werden können
 
         delegate void Notifier(String sender);
+        delegate void Adder(int x);
 
         static void SayHello(String sender)
         {
-            Console.WriteLine(sender);
+            Console.WriteLine("Hallo " + sender);
         }
 
         static void SayGoodBye(String sender)
         {
-            Console.WriteLine(sender);
+            Console.WriteLine("Tschüss " + sender);
+        }
+
+        void Add(int x)
+        {
+            Console.WriteLine(x + 2);
+        }
+
+        static void AddStatic(int x)
+        {
+            Console.WriteLine(x + 2);
         }
 
         static void Main(string[] args)
         {
             Notifier notify = new Notifier(SayHello);
-            notify("Hallo");
+            notify += new Notifier(SayGoodBye);
+            notify("Patrick");
+            Program prog_1 = new Program();
+            Program prog_2 = new Program();
+            Adder add = new Adder(prog_1.Add);
+            add += new Adder(prog_2.Add);
+            Console.WriteLine("Delegate calls the Method of the two Objects");
+            add(2);
+            Console.WriteLine("Remove first Method call");
+            add -= prog_1.Add;
+            add(2);
+            Console.WriteLine("Add static Method");
+            add += new Adder(Program.AddStatic);
+            add(2);
+            Console.WriteLine("Alternativ assignment");
+            Adder addIt = prog_1.Add;
+            addIt(2);
+            
         }
     }
 }
